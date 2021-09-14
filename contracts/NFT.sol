7d78182594ac66affbbf2389b1f30cff21262b62
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-14 16:47:45 -0500

    Upload smart contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,7 +5,41 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RareAnimals is ERC721Enumerable, Ownable {
+contract NFT is ERC721Enumerable, Ownable {
     using Strings for uint256;
-    
+
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public cost = 10000 ether; // Is equality as = 0.0001 matics each NFT
+    uint256 public maxSupply = 10000;
+    uint256 public maxMintAmount = 20;
+    bool public paused = false;
+    mapping(address => bool) public whitelisted;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI
+    ) ERC721(_name, _symbol) {
+        setBaseURI(_initBaseURI);
+        mint(msg.sender, 20);
+    }
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;        
+    }
+
+    function mint(address _to, uint256 _mintAmount) public payable {
+        uint256 supply = totalSupply();
+        require(!paused);
+        require(_mintAmount > 0);
+        require(_mintAmount <= maxMintAmount);
+        require(supply + _mintAmount <= maxSupply);
+
+        if(msg.sender != owner()) {
+            if(whitelisted[msg.sender] != true)
+        }
+    }
+
 }
